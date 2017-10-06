7c21c7fa4d2d0aa0a6e3436fabe60945959eb8fe
Lorant Lajos <trojanosall@gmail.com>
2017-10-06 11:26:55 +0200

    FavouriteAnimals starting

---
+++ FavouriteAnimals_Lajos_Lorant/FavouriteAnimals/FavouriteAnimals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FavouriteAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The program's aim is to collect your favourite animals and store them in a text file.
+            // There is a given text file called '''favourites.txt''', it will contain the animals
+            // If ran from the command line without arguments
+            // It should print out the usage:
+            // ```C# FavouriteAnimals [animal] [animal]```
+            // You can add as many command line arguments as many favourite you have.
+            // One animal should be stored only at once
+            // Each animal should be written in separate lines
+            // The program should only save animals, no need to print them
+
+        }
+    }
+}
