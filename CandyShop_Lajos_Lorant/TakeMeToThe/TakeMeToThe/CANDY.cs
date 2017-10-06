74aef284f4aadd9443b6b643f79d04f499fadea6
Lorant Lajos <trojanosall@gmail.com>
2017-10-06 13:16:18 +0200

    Candyshop - Candy class created

---
+++ CandyShop_Lajos_Lorant/TakeMeToThe/TakeMeToThe/CANDY.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeMeToThe
+{
+    class CANDY : CandyShop
+    {
+        public CANDY()
+        {
+            this.priceOfSweet = 20;
+            this.sugarNeededForSweets = 10;
+        }
+    }
+}
